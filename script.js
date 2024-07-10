b52942553f6a07e99a4bd781f560d0a820dd3f65
TDAS24 <dastautik12@gmail.com>
2024-07-10 14:47:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function menu (){
+    document.getElementById("p1").style.backgroundImage="url('https://img.cdn4dd.com/cdn-cgi/image/fit=cover,width=600,height=400,format=auto,quality=80/https://doordash-static.s3.amazonaws.com/media/store/header/4494.jpg')";
+    document.getElementById("p2").innerHTML="<table><tr><th>Name"
+   +"</th><th>price</th></tr>"
+   +"<tr><td>Panner Pizza</td><td>$10.99</td></tr>"
+   +"<tr><td>Cheese Pizza</td><td>$6.89</td></tr>"
+   +"<tr><td>Chicken Sanwich</td><td>$4.58</td></tr>"
+   +"<tr><td>Grilled Salmon</td><td>$20.99</td></tr>"
+ +"</table>"
+}
+function aboutus (){
+    document.getElementById("p1").style.backgroundImage="url('https://infographicjournal.com/wp-content/uploads/2016/01/Ginos-Pizza-Infographic1.jpg')"
+    document.getElementById("p2").innerHTML=""
+    +"<div>"
+    +"<h3>Our Address is 09398</h3>"
+    +"<h4>We make special pizza to our customers and serve hot and tasty to show how we serve our world's pizza</h4>"
+    +"</div"
+}
+function home (){
+    document.getElementById("p1").style.backgroundImage="url('https://tse3.mm.bing.net/th?id=OIP.d5abZmfJPkKGb9AxJ6-gFgHaE7&pid=Api&P=0&h=180')"
+    document.getElementById("p2").innerHTML=""
+    +"<h1>Welcome to pizza home</h1>"
+    +"BEST PIZZA"
+}
+function contactus (){
+    document.getElementById("p1").style.backgroundImage="url('https://www.mymarios.com/site/wp-content/uploads/1972/06/marios-contact-us.jpg')"
+    document.getElementById("p2").innerHTML="About Us"
+    +"<h2><i>Contact us 374-793-3493 and order the best pizza</i></h2>"
+}
